6fb95cf29d9544564418bf486efc022bbb96a12a
esantos-2019254 <esantos-2019254@kinal.edu.gt>
2024-02-23 11:34:32 -0600

    Se paso verificaciones de estudiante de controlador a validarCampos

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -33,40 +33,21 @@ const getStudentById = async (req, res) => {
 
 const studentsPost = async (req, res) => {
     const { nombre, apellido, correo, password, cursos } = req.body;
-    try {
 
         const student = new Student({ 
             nombre, 
             apellido, 
             correo, 
             password, 
-            cursos: []
+            cursos
         });
-
-        if (cursos && cursos.length > 0) {
-            for (let _id of cursos) {
-                const curso = await Curso.findById(_id);
-                if (!curso) {
-                    return res.status(400).json({ message: `El curso con ID ${_id} no existe` });
-                }
-                if (student.cursos.includes(_id)) {
-                    return res.status(400).json({ message: `El alumno ya está asignado al curso con ID ${_id}` });
-                }
-                if (student.cursos.length >= 3) {
-                    return res.status(400).json({ message: 'El alumno ya está asignado a 3 cursos' });
-                }
-                student.cursos.push(_id);
-            }
-        }
 
         await student.save();
         res.status(200).json({
             student
         });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error al crear el alumno' });
-    }
+        /*console.error(error);
+        res.status(500).json({ message: 'Error al crear el alumno' });*/
 }
 
 const studentsPut = async (req, res) => {
--- middlewares/validarCampos.js
+++ middlewares/validarCampos.js
@@ -1,11 +1,35 @@
 const { validationResult } = require('express-validator');
 
+const Curso = require('../models/curso');
+const Student = require('../models/student');
+
 const validarCampos = (req, res, next) =>{
     const error = validationResult(req);
     if(!error.isEmpty){
         return res.status(400).json(error);
     }
 
+
+    const { cursos } = req.body
+
+    const student = new Student();
+
+    if (cursos && cursos.length > 0) {
+        for (let _id of cursos) {
+            const curso = Curso.findById(_id);
+            if (!curso) {
+                return res.status(400).json({ message: `El curso con ID ${_id} no existe` });
+            }
+            if (student.cursos.includes(_id)) {
+                return res.status(400).json({ message: `El alumno ya está asignado al curso con ID ${_id}` });
+            }
+            if (student.cursos.length >= 3) {
+                return res.status(400).json({ message: 'El alumno ya está asignado a 3 cursos' });
+            }
+            student.cursos.push(_id);
+        }
+    }
+    
     next();
 }
 
